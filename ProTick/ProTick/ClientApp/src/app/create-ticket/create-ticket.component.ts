5f15a7ebc8f514cb5e4caa335c0d4c47d513b0f2
Jonas <schildberger.jonas@gmail.com>
2019-07-04 15:51:55 +0200

    create-ticket component erstellt

---
+++ ProTick/ProTick/ClientApp/src/app/create-ticket/create-ticket.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Ticket } from '../../classes/ticket'
+
+@Component({
+  selector: 'app-create-ticket',
+  templateUrl: './create-ticket.component.html',
+  styleUrls: ['./create-ticket.component.css']
+})
+export class CreateTicketComponent implements OnInit {
+  
+  public Ticket
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ProTick/ProTick/ClientApp/src/classes/Ticket.ts
@@ -0,0 +1,6 @@
+export interface Ticket {
+  TicketID: number,
+  Description: string,
+  StateFK: number,
+  SubprocessFK: number
+}
