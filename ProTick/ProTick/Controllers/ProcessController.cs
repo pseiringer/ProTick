4d66b9cfbf024980e3e1707aa47348ac16560997
lhaselmayr <lisa.haselmayr2000@gmail.com>
2019-07-08 10:49:22 +0200

    created first few easy methods

--- ProTick/ProTick/Controllers/ProcessController.cs
+++ ProTick/ProTick/Controllers/ProcessController.cs
@@ -18,10 +18,28 @@ namespace ProTick.Controllers
 
         public List<Process> Test([FromServices] ProTickDatabaseContext db)
         {
-            db.Process.Add(new Process { Description = "test_process_1"});
+            db.Process.Add(new Process { Description = "test_process_1" });
             db.Process.Add(new Process { Description = "test_process_2" });
             db.SaveChanges();
 
+            return db.Process.ToList();
+        }
+
+        [HttpGet("Process/{id}")]
+        public Process GetProcess([FromServices] ProTickDatabaseContext db, int id)
+        {
+            return db.Process.First(x => x.ProcessID == id);
+        }
+
+        [HttpGet("Process")]
+        public List<Process> GetProcesses([FromServices] ProTickDatabaseContext db)
+        {
+            return db.Process.ToList();
+        }
+
+        [HttpPost("Process")]
+        public List<Process> NewProcess([FromServices] ProTickDatabaseContext db)
+        {
             return db.Process.ToList();
         }
     }
