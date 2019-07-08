fd216a5df60ebf2b63e08bf7c84a63200711e652
Jonas <schildberger.jonas@gmail.com>
2019-07-08 14:00:44 +0200

    started testing angular connection

--- ProTick/ProTick/ClientApp/src/app/app.module.ts
+++ ProTick/ProTick/ClientApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-//import { CreateTicketComponent } from './create-ticket/create-ticket.component';
+import { CreateTicketComponent } from './create-ticket/create-ticket.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -18,17 +20,19 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    //CreateTicketComponent,
+    CreateTicketComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    HttpModule,
     FormsModule,
+    CoreModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-     // { path: 'create-ticket', component: CreateTicketComponent },
+      { path: 'create-ticket', component: CreateTicketComponent },
     ])
   ],
   providers: [],
--- ProTick/ProTick/ClientApp/src/app/create-ticket/create-ticket.component.ts
+++ ProTick/ProTick/ClientApp/src/app/create-ticket/create-ticket.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Ticket } from '../../classes/ticket'
+import { Process } from '../../classes/Process';
+import { ProcessDataService } from '../core/process/process-data.service';
 
 @Component({
   selector: 'app-create-ticket',
   templateUrl: './create-ticket.component.html',
-  styleUrls: ['./create-ticket.component.css']
+  styleUrls: ['./create-ticket.component.css'],
+  providers: [ProcessDataService]
 })
+
 export class CreateTicketComponent implements OnInit {
-  
-  public Ticket
+  allProcesses: any = [];
 
-  constructor() { }
+  process: Process = {
+    ProcessID: null,
+    Description: null
+  };
 
-  ngOnInit() {
+  constructor(private _processDataService: ProcessDataService) { }
+
+  ngOnInit() { }
+
+  getProcesses() {
+    this._processDataService.getProcesses()
+      .subscribe(data => this.allProcesses = data);
+
+    console.log(this.allProcesses);
   }
 
+  postProcess() {
+    this.process.Description = "Beschreibung_1";
+
+    this._processDataService.postProcess(this.process);
+  }
 }
